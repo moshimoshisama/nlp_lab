3dbcc72b4c279bab3c5efae5059c9af4c3ef7aa2
Jingpei <jingpei.wu@tum.de>
2021-06-01 18:17:34 +0200

    upload files for unofficial code in Colab

--- LeverageJustAFewKeywords/config.py
+++ LeverageJustAFewKeywords/config.py
@@ -6,11 +6,13 @@ hparams = {
         'pretrained_dim': 768,
         'num_aspect': 9,
     },
-    'description': 'bag baseline',
-    'save_dir': './ckpt/bags',
-    'aspect_init_file': './data/seedwords/bags_and_cases.30.txt',
-    'train_file': './data/bags_train.json',
-    'test_file': './data/bags_test.json',
+    'description': 'bag_and_cases baseline',
+    'save_dir': './LeverageJustAFewKeywords/ckpt/bags_and_cases',
+    'aspect_init_file': './LeverageJustAFewKeywords/data/bags_and_cases.30.txt',
+    'train_file': './LeverageJustAFewKeywords/data/bags_and_cases_train.json',
+    'test_file': './LeverageJustAFewKeywords/data/bags_and_cases_test.json',
     'general_asp': 4,
     'maxlen': 40
 }
+
+# in local machine/gitlab repo, LeverageJustAFewKeywords is not the root folder
